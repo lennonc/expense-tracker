8891658805a26fc84d5fea98eedf87c8bd38d6db
Lennon Chimbumu <lennonc@users.noreply.github.com>
2021-02-16 21:58:06 +0200

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/components/HomeScreen';
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
